d222b0acd46d3973a5f4ba22ece124ac1d663a00
Larissa Gimenez <laris.gimenez13@gmail.com>
2026-06-17 12:00:38 -0300

    feat: implement Clients module with model, routes, and service provider registration

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,10 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\FortifyServiceProvider;
+use Modules\Clients\Providers\ClientsModuleServiceProvider;
 
 return [
     AppServiceProvider::class,
     FortifyServiceProvider::class,
+    ClientsModuleServiceProvider::class
 ];
---
+++ modules/clients/Providers/ClientsModuleServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\Clients\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ClientsModuleServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../Routes/api.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/clients.php',
+            'clients'
+        );
+    }
+}
